31c745ec4345b6c2d00e1d33f53f5585defa57d1
pranavkneeraj <pranav.k@amazatic.com>
2017-05-25 21:27:13 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -145,6 +145,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'pranavkchaudhary@gmail.com'
-EMAIL_HOST_PASSWORD = 'pvwswhfwuberlirg'
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = 'pranavkchaudhary@gmail.com'
 EMAIL_PORT = 587
